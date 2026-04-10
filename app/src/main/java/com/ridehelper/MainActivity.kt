4e69d684d613195d6bceaa617a8ad0f0761b7df2
dieghenr22-maker <dieg.henr22@gmail.com>
2026-04-09 23:32:24 -0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/ridehelper/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ridehelper
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://SEU-APP-AQUI.com") // ALTERAR
+    }
+}
